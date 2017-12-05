5b68b2ead68fd315f00c2500aced300e84226cd5
Panuwich Phetklom <panuwich123@gmail.com>
2017-12-05 14:11:47 +0700

    b

---
+++ app/src/main/java/com/example/minesweeper/how_to_play.java
@@ -0,0 +1,14 @@
+package com.example.minesweeper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class how_to_play extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_how_to_play);
+        getSupportActionBar().setTitle("How To Play");
+    }
+}
